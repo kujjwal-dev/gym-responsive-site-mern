f6ee85de96cd9b494d748f718b73b1e8508b65f8
ujjwal <khannaujjwal21@gmail.com>
2024-05-29 10:59:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Hero from './components/hero/Hero'
+
+function App() {
+
+
+  return (
+    <>
+      <Hero/>
+    </>
+  )
+}
+
+export default App
